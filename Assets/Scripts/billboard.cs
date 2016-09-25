560f06c4f5312f9de2ed2858a93dae1622f6def8
heromi <ninsatu@gmail.com>
2016-09-26 00:03:57 +0900

    ・紅葉モデルの追加 ・スクリプトにbillboardを追加 ・モデル類をMeshesフォルダ内へ移動

---
+++ Assets/Scripts/billboard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class billboard : MonoBehaviour {
+
+	public Transform target_transform_;
+
+	// Use this for initialization
+	void Start () {
+
+		if(target_transform_ == null) {
+			//MainCameraをターゲットとする
+			target_transform_ = GameObject.FindGameObjectWithTag("MainCamera").transform;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//カメラの方向を向くようにする
+		this.transform.LookAt(target_transform_.position);
+	}
+}
+
